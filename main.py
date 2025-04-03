08dbc086c8055f078d5ab059c5a113ec5cff1137
Javier Rojas <rojasj84@gmail.com>
2025-04-03 15:10:14 -0400

    4/3/2025

--- main.py
+++ main.py
@@ -16,7 +16,6 @@ if __name__ == "__main__":
     #***** Building USER GUI *****
 
     # Begin code with window code
-    #This is a code test 
     window = tk.Tk()
     window.title("EPL Laser Heating Control")
     window.geometry("1800x1010")
